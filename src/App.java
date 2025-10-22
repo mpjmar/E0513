4b84ab002d2f9ac18d0adb427c127bd2db713f06
m. paz jimenez <meripeich@gmail.com>
2025-10-22 19:04:12 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+		int positivos = 0;
+		int negativos = 0;
+
+		System.out.println("Por favor, introduzca 10 números enteros: ");
+		for (int i = 0; i < 10; i++)
+		{
+			try {
+				int n = Integer.parseInt(System.console().readLine());
+				//n < 0 ? positivos++ : negativos++; //???????????????????????
+				if (n < 0) negativos++;
+				else positivos++;
+			}
+			catch (NumberFormatException e) {
+				System.out.println("Por favor, introduce un número entero");
+			}
+			catch (Exception e) {
+				System.out.println("Ha ocurrido un error inesperado");
+			}
+		}
+		System.out.printf("Ha introducido %d positivos y %d negativos%n", positivos, negativos);
+    }
+}
